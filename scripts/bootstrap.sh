da2168aeb8f0c6e00c857b474880fb6a57172860
Abhiraj <abhirajkv@outlook.com>
2026-07-25 11:00:28 +0530

    test

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -26,17 +26,16 @@
 HOSTNAME=$(/usr/bin/curl -s http://169.254.169.254/latest/meta-data/tags/instance/Name);/usr/bin/hostnamectl --static set-hostname $HOSTNAME
 
 # Network Static IP configuration
-DEV=$(/usr/bin/nmcli con show|egrep -v "loopback|DEVICE"|awk '{print $4}')
+DEV=$(/usr/bin/nmcli con show|egrep -v "loopback|DEVICE"|awk '{print $5}')
 IP=$(/usr/sbin/ip a|grep -i inet|grep ens5|awk '{print $2}')
 GW=$(/usr/bin/netstat -nr| awk '{print $2}' |egrep -v "IP|Gateway|0.0.0.0")
 DNS=$(/usr/bin/cat /etc/resolv.conf |grep nameserver|awk '{print $2}')
 
 /usr/bin/nmcli connection modify "cloud-init ens5" connection.id "ens5"
 /usr/bin/nmcli connection modify ens5 ipv6.method disabled
-/usr/bin/nmcli connection modify ens5 ipv6.method disabled
-/usr/bin/nmcli connection down "cloud-init ens5";/usr/bin/nmcli connection up "ens5"
+/usr/bin/nmcli connection down "ens5";/usr/bin/nmcli connection up "ens5"
 
 /usr/bin/nmcli con mod "$DEV" ipv4.addresses "$IP" ipv4.method manual
 /usr/bin/nmcli con mod "$DEV" ipv4.gateway "$GW"
 /usr/bin/nmcli con mod "$DEV" ipv4.dns "$DNS"
-/usr/bin/nmcli con up "$DEV" 
+/usr/bin/nmcli connection down "$DEV";/usr/bin/nmcli connection up "$DEV"
--- scripts/cluster_setup.sh
+++ scripts/cluster_setup.sh
@@ -1,11 +1,15 @@
 /usr/bin/dnf install -y pacemaker corosync pcs fence-agents-all
 /usr/bin/systemctl enable pcsd --now
-/usr/bin/echo R3dh@tPa55 |/usr/bin/passed hacluster --stdin
+/usr/bin/echo R3dh@tPa55 | /usr/bin/passwd hacluster --stdin
 ServerName=$(hostname)
+ServerCount=$(cat /etc/hosts|grep server|wc -l)
 if [ "$ServerName" == "server00" ]; then
-  /usr/sbin/pcs cluster auth server00 server01 -u hacluster -p R3dh@tPa55 --force
-  /usr/sbin/pcs cluster setup --name webserver server00 server01
-  /usr/sbin/pcs cluster start --all
-  /usr/sbin/pcs property set stonith-enabled=false
-  /usr/bin/pcs property set no-quorum-policy=ignore
+    sleep 120
+    for ((i=0; i<ServerCount; i++)); do
+      /usr/sbin/pcs host auth server0$i -u hacluster -p R3dh@tPa55
+    done
+  /usr/sbin/pcs cluster setup webserver --start server00 server01
+  /usr/sbin/pcs cluster enable --all
+  /usr/sbin/pcs property set stonith-enabled=true
+  /usr/sbin/pcs property set no-quorum-policy=ignore
 fi
